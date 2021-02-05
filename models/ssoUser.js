9ae0550b6b0f6e8100ad01783cc197606bfc140c
gKushagra <55290773+gKushagra@users.noreply.github.com>
2021-02-05 15:28:28 -0600

    fixed bcrypt dependency

--- models/ssoUser.js
+++ models/ssoUser.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const { Schema } = mongoose;
