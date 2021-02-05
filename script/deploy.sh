a47679662a698c21bd610d65c0af521408836035
gKushagra <55290773+gKushagra@users.noreply.github.com>
2021-02-05 17:52:35 -0600

    fixed npm deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cd /var/myapps/oddfelow-sso
-sudo pm2 stop oddfellow-sso-api
-sudo git pull origin main
-sudo npm install
-sudo pm2 restart oddfellow-sso-api
+pm2 stop oddfellow-sso-api
+git pull origin main
+npm install
+pm2 restart oddfellow-sso-api
 exit
 EOF
