f3067caf071345b1e2d66a702d297af9f1419304
gKushagra <55290773+gKushagra@users.noreply.github.com>
2021-02-05 18:36:30 -0600

    testing build changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // for integration test
 app.get("/test", function (req, res) {
-  res.send('Hi! You have reached OddFellow SSO REST API Test endpoint. <a href="https://sso.oddfellow.in">Visit Website</a>');
+  res.send('Hi! You have called OddFellow SSO REST API Test endpoint. <a href="https://sso.oddfellow.in">Visit Website</a>');
 });
 
 // configure app
