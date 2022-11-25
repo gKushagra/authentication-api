8ffc48eb2abf271ca311a3f72435dbf64cf993d6
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-11-24 23:41:14 -0600

    update api status controller

--- src/controllers/statusController.js
+++ src/controllers/statusController.js
@@ -3,15 +3,10 @@ const getApiStatusCommand = require('../commands/getApiStatusCommand');
 const date = new Date();
 
 const getApiStatusController = async (req, res) => {
-    try {
-        logger.info(`${date.getUTCDate()}:: getApiStatusController called`);
-        const result = await getApiStatusCommand();
-        logger.info(`${date.getUTCDate()}:: getApiStatusController returning response`);
-        return res.status(200).json(result);
-    } catch (error) {
-        logger.error(`${date.getUTCDate()}:: getApiStatusController Error: ${error}`);
-        return res.sendStatus(500);
-    }
+    logger.info(`${date.getUTCDate()}:: getApiStatusController called`);
+    const result = unwrapResult(await getApiStatusCommand());
+    logger.info(`${date.getUTCDate()}:: getApiStatusController done`);
+    res.status(result.code).json(result.data);
 }
 
 module.exports = {
